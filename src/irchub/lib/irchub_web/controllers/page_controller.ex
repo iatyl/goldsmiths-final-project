6ab25e59472040b15be71f8feb09b761132648b0
zwang038@gold.ac.uk <zwang038@gold.ac.uk>
2024-02-15 06:56:19 +0000

    add functions to return login url and homepage url

--- src/irchub/lib/irchub_web/controllers/page_controller.ex
+++ src/irchub/lib/irchub_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule IrchubWeb.PageController do
   use IrchubWeb, :controller
   @login_path "/users/log_in/"
   @homepage_path "/hub/"
+  def login_path, do: @login_path
+  def homepage_path, do: @homepage_path
   def home(conn, _params) do
     current_user = conn.assigns[:current_user]
     redir_path =
