24b3dd7388cd34ba985a9bb7aade346f9d5881e9
zwang038@gold.ac.uk <zwang038@gold.ac.uk>
2024-04-02 14:53:58 +0000

    update forget password page

--- v1/irchub/lib/irchub_web/live/user_forgot_password_live.ex
+++ v1/irchub/lib/irchub_web/live/user_forgot_password_live.ex
@@ -5,20 +5,23 @@ defmodule IrchubWeb.UserForgotPasswordLive do
 
   def render(assigns) do
     ~H"""
+    <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
+    <.input field={@form[:email]} type="email" placeholder="Email" required />
+    <:actions>
+      <.button phx-disable-with="Sending..." class="w-full">
+        Send password reset instructions
+      </.button>
+    </:actions>
+    </.simple_form>
+
+    """
+    ~H"""
     <div class="mx-auto max-w-sm">
       <.header class="text-center">
         Forgot your password?
-        <:subtitle>We'll send a password reset link to your inbox</:subtitle>
+        <:subtitle>We won't send a password reset link to your inbox :3</:subtitle>
       </.header>
 
-      <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
-        <.input field={@form[:email]} type="email" placeholder="Email" required />
-        <:actions>
-          <.button phx-disable-with="Sending..." class="w-full">
-            Send password reset instructions
-          </.button>
-        </:actions>
-      </.simple_form>
       <p class="text-center text-sm mt-4">
         <.link href={~p"/users/register"}>Register</.link>
         | <.link href={~p"/users/log_in"}>Log in</.link>
